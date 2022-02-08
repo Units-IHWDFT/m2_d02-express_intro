59071d607eca162d7551903f64dc72d146668247
marcosantonastasi <marco.santonastasi@gmail.com>
2022-02-08 07:50:05 +0100

    cleanup app.js file from comments

--- app.js
+++ app.js
@@ -1,41 +1,20 @@
-//   name you give	   name of the package you installed
-//       |                  |
-const express = require('express');
+const express = require("express");
 
-require('dotenv').config();
+require("dotenv").config();
 
-const PORT = process.env.PORT || 3000; //  Set default port to 3000 if port is not defined
-
-// here we are creating application
 const app = express();
 
-// ///////////////////////////////////////////////////////////////////////////////////////
-// stage 2:
-// Connect "public/" folder to our express app
-// makes everything inside public folder accessible to the rest of the app
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-// ///////////////////////////////////////////////////////////////////////////////////////
+app.get("/", (req, res, next) => res.send("Hello there!"));
 
-// ***********************************
-// ROUTES
-// ***********************************
-
-//      '/' => means localhost:3000
-//       ^
-//       |    callback function as 2nd argument
-app.get('/', (req, res, next) => res.send('Hello there!'));
-
-// ///////////////////////////////////////////////////////////////////////////////////////
-// stage 2:
-// localhost:3000/homepage
-app.get('/homepage', (req, res, next) =>
-  res.sendFile(__dirname + '/views/home.html')
+app.get("/homepage", (req, res, next) =>
+  res.sendFile(__dirname + "/views/home.html")
 );
 
-app.get('/about', (req, res, next) =>
-  res.sendFile(__dirname + '/views/about.html')
+app.get("/about", (req, res, next) =>
+  res.sendFile(__dirname + "/views/about.html")
 );
-// ///////////////////////////////////////////////////////////////////////////////////////
 
-app.listen(PORT, () => console.log('🏃‍ on port 3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log("🏃‍ on port 3000"));
